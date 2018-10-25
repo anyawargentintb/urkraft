a6b6b00e6ea8f364fd98796658caa269887d6712
Unknown <anya.wargentin@trafikbyran.com>
2018-10-25 16:53:08 +0200

    First commit.

---
+++ includes/loop-index.php
@@ -0,0 +1,30 @@
+
+<div class="post-wrapper">
+
+	<div class="content-wrapper">
+		hej
+	</div>
+
+	<p>Taggar:</p> 
+	<div class="post-contact-wrapper">
+		<form id="contact-form" method="post" action="">
+			<div class="contact-field">
+				<label for="name">För- och efternamn</label>
+				<input type="text" id="name" name="name" required>
+			</div>
+			<div class="contact-field">
+				<label for="phone">Telefonnummer</label>
+				<input type="text" id="phone" name="phone" required>
+			</div>
+			<div class="contact-field">
+				<label for="email">E-postadress</label>
+				<input type="text" id="email" name="email" required>
+			</div>
+			<div class="contact-field">
+				<label for="message">Meddelande</label>
+				<textarea name="email" required></textarea>
+			</div>
+		</form>
+	</div>
+</div>
+
