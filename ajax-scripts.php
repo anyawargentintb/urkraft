468de1c5ab99ec70d0a38d9a016a3b65df5fa0e6
Filip <filip@effektid.com>
2018-10-25 17:16:02 +0200

    jquery

--- ajax-scripts.php
+++ ajax-scripts.php
@@ -1,3 +1,31 @@
 <script>
-	
+	jQuery(document).ready( function($) {
+		function getSingle(e)  {
+			// var target = $(this);
+			// var key = target.attr('data-product_key');
+			$.ajax({
+				type: 'POST',
+				// url: adminUrl,
+				data: {
+					'text' : 'hej',
+					'action': 'wp_ajax_get_singel' //this is the name of the AJAX method called in WordPress
+				}, 
+				success: function (result) {
+				 	
+				 	console.log('getSingle - success');
+
+				 	console.log(result);
+
+				},
+				error: function () {
+
+					 console.log('getSingle - fail');
+					
+				}
+			});
+
+			console.log('getSingle');
+
+		}
+	});
 </script>
